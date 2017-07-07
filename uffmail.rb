2758fade36a6a2ca281e8f60a7d8c8a1c4fc0777
Daniel Callado <dcallado@id.uff.br>
2017-07-07 12:09:06 -0300

    Inicializando a função gera_email

--- uffmail.rb
+++ uffmail.rb
@@ -4,6 +4,14 @@ require_relative 'db'
 class UFFMail
 
   def gera_email(aluno, arquivo)
+    nomes = aluno.nome.split(" ")
+    ok = false
+    contador = 1
+    lista = []
 
+    #Opção 1 Primeira letra (ou mais) do primeiro nome + ultimo nome
+    while !ok
+      novo = 
+    end
   end
 end
